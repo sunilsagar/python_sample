2558c7e64113cc03ea365dd55acaf6ff0f36d6a0
sunilsagar <sunilsagar31@gmail.com>
2020-09-23 15:12:43 +0800

    final copy

--- tut8.py
+++ tut8.py
@@ -1,3 +1,12 @@
 mystr = "This is learning day"
 print(mystr[0:20])
 print(len(mystr))
+print(mystr[0:20:2])
+print(mystr[-4:-2])
+print(mystr.isalnum())
+print(mystr.endswith("day"))
+print(mystr.count("i"))
+print(mystr.find("is"))
+print(mystr.capitalize())
+print(mystr.upper())
+print(mystr.replace("is", "are"))
