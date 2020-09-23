bcb6f4b16ef41fd0cfde1af56c777921e077bcd2
sunilsagar <sunilsagar31@gmail.com>
2020-09-23 15:53:52 +0800

    Adding File 9 and 10

---
+++ tut9.py
@@ -0,0 +1,25 @@
+grocery = ["Harpic", "vim bar", "deodrant", "Bhindi", "Lollypop", 56]
+print(grocery)
+print(grocery[1])
+numbers = [ 2, 7, 8, 11, 9]
+print(numbers)
+print(numbers[2])
+#numbers.sort()
+#numbers.reverse()
+print(numbers)
+print(numbers[:5])
+print(numbers)
+print(numbers[::2])
+numbers.append(1)
+numbers.insert(2, 67)
+numbers.remove(9)
+numbers.pop()
+print(numbers)
+
+tp = (1, 2, 3)
+print(tp)
+
+a = 1
+b = 8
+a, b = b, a
+print(a, b)
